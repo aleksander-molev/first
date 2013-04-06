59b9e25533009d31cf806261ee92fd8d0e5bf299
aleksander-molev <aleksander-molev@yandex.ru>
2013-04-06 21:09:58 +0400

    f

--- Test.java
+++ Test.java
@@ -5,5 +5,7 @@ class Test
    public static void main(Sting[] args)
    {
      System.out.println("Hello World!");
+     System.out.println("End of files!");
+     System.out.println("Test 1");
    }
 }
